30d5bf5304f2f16b624af4b8977360a06ae4b293
peko <peko@gasubasu.com>
2015-12-02 03:34:34 +0300

    update

--- src/mesh.c
+++ src/mesh.c
@@ -94,7 +94,7 @@ void mesh_shape(int shape_id) {
 
     // triangulate("pczAevn", &in, &mid, &vorout);
 
-    triangulate("pzq30a2", &in, &mesh, (struct triangulateio *) NULL);
+    triangulate("pzq30a10", &in, &mesh, (struct triangulateio *) NULL);
 
     meshX    = (double *) malloc(mesh.numberofpoints * sizeof(double));
     meshY    = (double *) malloc(mesh.numberofpoints * sizeof(double));
@@ -170,99 +170,4 @@ void mesh_free() {
     free(meshY);    
     free(pr_meshX);
     free(pr_meshY);
-}
-
-static void report(io, markers, reporttriangles, reportneighbors, reportsegments,
-            reportedges, reportnorms)
-struct triangulateio *io;
-int markers;
-int reporttriangles;
-int reportneighbors;
-int reportsegments;
-int reportedges;
-int reportnorms;
-{
-  int i, j;
-
-  for (i = 0; i < io->numberofpoints; i++) {
-    printf("Point %4d:", i);
-    for (j = 0; j < 2; j++) {
-      printf("  %.6g", io->pointlist[i * 2 + j]);
-    }
-    if (io->numberofpointattributes > 0) {
-      printf("   attributes");
-    }
-    for (j = 0; j < io->numberofpointattributes; j++) {
-      printf("  %.6g",
-             io->pointattributelist[i * io->numberofpointattributes + j]);
-    }
-    if (markers) {
-      printf("   marker %d\n", io->pointmarkerlist[i]);
-    } else {
-      printf("\n");
-    }
-  }
-  printf("\n");
-
-  if (reporttriangles || reportneighbors) {
-    for (i = 0; i < io->numberoftriangles; i++) {
-      if (reporttriangles) {
-        printf("Triangle %4d points:", i);
-        for (j = 0; j < io->numberofcorners; j++) {
-          printf("  %4d", io->trianglelist[i * io->numberofcorners + j]);
-        }
-        if (io->numberoftriangleattributes > 0) {
-          printf("   attributes");
-        }
-        for (j = 0; j < io->numberoftriangleattributes; j++) {
-          printf("  %.6g", io->triangleattributelist[i *
-                                         io->numberoftriangleattributes + j]);
-        }
-        printf("\n");
-      }
-      if (reportneighbors) {
-        printf("Triangle %4d neighbors:", i);
-        for (j = 0; j < 3; j++) {
-          printf("  %4d", io->neighborlist[i * 3 + j]);
-        }
-        printf("\n");
-      }
-    }
-    printf("\n");
-  }
-
-  if (reportsegments) {
-    for (i = 0; i < io->numberofsegments; i++) {
-      printf("Segment %4d points:", i);
-      for (j = 0; j < 2; j++) {
-        printf("  %4d", io->segmentlist[i * 2 + j]);
-      }
-      if (markers) {
-        printf("   marker %d\n", io->segmentmarkerlist[i]);
-      } else {
-        printf("\n");
-      }
-    }
-    printf("\n");
-  }
-
-  if (reportedges) {
-    for (i = 0; i < io->numberofedges; i++) {
-      printf("Edge %4d points:", i);
-      for (j = 0; j < 2; j++) {
-        printf("  %4d", io->edgelist[i * 2 + j]);
-      }
-      if (reportnorms && (io->edgelist[i * 2 + 1] == -1)) {
-        for (j = 0; j < 2; j++) {
-          printf("  %.6g", io->normlist[i * 2 + j]);
-        }
-      }
-      if (markers) {
-        printf("   marker %d\n", io->edgemarkerlist[i]);
-      } else {
-        printf("\n");
-      }
-    }
-    printf("\n");
-  }
 }
